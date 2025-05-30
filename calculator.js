444ef1d9540a7ba9511e5b1548f004b5f29256e0
dsant2119 <143536409+dsant2119@users.noreply.github.com>
2025-05-30 15:52:34 +0900

    Link js file and create initial math functions and operate function

--- calculator.js
+++ calculator.js
@@ -1,1 +1,30 @@
 console.log("Hello world!");
+
+//  CREATE ADD SUBTRACT MULTIPLY DIVIDE FUNCTIONS
+
+const add = (num1, num2) => {
+  return num1 + num2;
+};
+
+const subtract = (num1, num2) => {
+  return num1 - num2;
+};
+
+const multiply = (num1, num2) => {
+  return num1 * num2;
+};
+
+const divide = (num1, num2) => {
+  if (num2 === 0) return undefined;
+  else return num1 / num2;
+};
+
+const operate = (operator, num1, num2) => {
+  if (operator === "+") return add(num1, num2);
+  if (operator === "-") return subtract(num1, num2);
+  if (operator === "*") return multiply(num1, num2);
+  if (operator === "/") return divide(num1, num2);
+  else return "INVALID OPERATOR";
+};
+
+console.log(operate("+", 2, 3));
